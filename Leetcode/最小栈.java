314c2c616d173e0ea0bf0909ecce07603222da5b
wangbo24 <1742008106@qq.com>
2019-12-08 14:37:24 +0800

    add

---
+++ Leetcode/最小栈.java
@@ -0,0 +1,35 @@
+public class MinStack {
+    Stack<Integer> stack;
+    Stack<Integer> minStack;
+    /** initialize your data structure here. */
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int x) {
+        stack.push(x);
+        if(minStack.isEmpty()) {
+            minStack.push(x);
+        }else{
+            if(minStack.peek() >= x){
+                minStack.push(x);
+            }
+        }
+    }
+
+    public void pop() {
+        if(stack.peek().equals(minStack.peek())){
+            minStack.pop();
+        }
+        stack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
