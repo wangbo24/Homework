4ca3b592c6a3fdb42ea52c8c921c045f62659c48
wangbo24 <1742008106@qq.com>
2019-12-23 22:00:15 +0800

    add

---
+++ Leetcode/101.java
@@ -0,0 +1,12 @@
+public boolean isSym(TreeNode left,TreeNode right){
+        if(left == null && right == null)
+            return true;
+        if(left == null || right == null)
+            return false;
+        return left.val == right.val && isSym(left.left,right.right) && isSym(left.right,right.left);
+    }
+    public boolean isSymmetric(TreeNode root) {
+        if(root == null)
+            return true;
+        return isSym(root.left, root.right);
+    }
