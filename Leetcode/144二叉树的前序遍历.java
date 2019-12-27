2cc6a0ad65c04b374609f0816667a515874d6565
wangbo24 <1742008106@qq.com>
2019-12-27 21:37:18 +0800

    add Leetcode/144二叉树的前序遍历.java

---
+++ Leetcode/144二叉树的前序遍历.java
@@ -0,0 +1,9 @@
+List<Integer> list = new LinkedList<>();
+    public List<Integer> preorderTraversal(TreeNode root) {
+        if(root != null){
+            list.add(root.val);
+            preorderTraversal(root.left);
+            preorderTraversal(root.right);
+        }
+        return list;
+    }
