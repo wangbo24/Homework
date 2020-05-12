68a0218b11e9a4c401493e0162f682aec791cff0
wangbo24 <1742008106@qq.com>
2020-05-12 21:56:37 +0800

    add

---
+++ 每日一题/day29.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main20 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] str = {};
+        int i = 0;
+        String[] lin = {};
+        while(sc.hasNext()){
+            str[i] = sc.nextLine();
+            lin = str[i].split(" ");
+        }
+
+    }
+}
