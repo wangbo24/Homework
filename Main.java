763252cc70b343160a3ea4c2b88b122406cab6fc
wangbo24 <1742008106@qq.com>
2020-06-08 23:00:10 +0800

    add

--- Main.java
+++ Main.java
@@ -1,13 +1,77 @@
-public class Main {
+import java.util.*;
+
+class Node{
+    char value;
+    Node left;
+    Node right;
+    public Node(char value){
+        this.value = value;
+    }
+}
+
+
+public class Main{
+
+    static int i = 0;
+    public static Node buildTree(String s){
+        if(s.charAt(i) != '#'){
+            Node root = new Node(s.charAt(i));
+            ++i;
+            root.left = buildTree(s);
+            ++i;
+            root.right = buildTree(s);
+            return root;
+        }
+        return null;
+    }
+
+    public static void inOrder(Node root){
+        if(root != null){
+            inOrder(root.left);
+            System.out.print(root.value + " ");
+            inOrder(root.right);
+        }
+    }
 
     public static void main(String[] args) {
-        Animal n = new Animal();
-        Animal n1 = new Pig();
-        Animal n2 = new Dog();
-        Animal n3 = new Human();
-        n.scream();
-        n1.scream();
-        n2.scream();
-        n3.scream();
+        //获取输入
+        /*
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+        */
+         String line = "cf#g#ny#h#t##hpg##h#e##oebr####h##r##";
+        //建树
+        Node root = buildTree(line);
+        //中序遍历
+        inOrder(root);
+        System.out.println();
+    }
+
+}
+
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> Mat = new ArrayList<>();
+        Queue<TreeNode> queue  = new LinkedList<>();
+        if(root != null)
+            queue.offer(root);
+        //层序遍历
+        while(!queue.isEmpty()){
+            //取出队列中的全部元素
+            int size = queue.size();
+            List<Integer> rowNode = new ArrayList<>();
+            while(size > 0){
+                TreeNode topNode = queue.poll();
+                //放在同一行
+                rowNode.add(topNode.val);
+                if(topNode.left != null)
+                    queue.offer(topNode.left);
+                if(topNode.right != null)
+                    queue.offer(topNode.right);
+                --size;
+            }
+            Mat.add(rowNode);
+        }
+        return Mat;
     }
 }
---
+++ Main1.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Animal n = new Animal();
+        Animal n1 = new Pig();
+        Animal n2 = new Dog();
+        Animal n3 = new Human();
+        n.scream();
+        n1.scream();
+        n2.scream();
+        n3.scream();
+    }
+}
